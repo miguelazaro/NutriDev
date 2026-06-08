21f6ae67832104a227d5e3ecd0feec0f47848651
miguelazaro <a3522110262@alumno.uttehuacan.edu.mx>
2026-06-08 15:12:47 -0600

    minor changes

--- middlewares/allowIfSuccess.js
+++ middlewares/allowIfSuccess.js
@@ -1,5 +1,5 @@
 module.exports = function allowIfSuccess(req, res, next) {
-  if (req.query.ok === '1' || req.query.ok === '0') return next();s
+  if (req.query.ok === '1' || req.query.ok === '0') return next();
   if (req.session?.usuario) return next();
-
+  return res.redirect('/login');
 };
